af7fe688fb34d9f102007ea2c19bb6c3b6b88646
Zachary1748 <zachary1748@fullsail.edu>
2016-08-21 11:58:37 -0500

    Initial commit; new project.

---
+++ ConnectedApplication/app/src/main/java/com/example/zachary/connectedapplication/MainActivity.java
@@ -0,0 +1,17 @@
+// Zachary Gover
+// JAV1 - 1608
+// MainActivity
+
+package com.example.zachary.connectedapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
